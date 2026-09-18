0836749c26290c05757c366e8286ec130b0dc188
Gita2903 <maulidigita29@gmail.com>
2026-09-19 01:29:15 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
 };
 
 module.exports = withNextIntl(nextConfig);
